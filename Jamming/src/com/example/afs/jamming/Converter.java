feeef0395c7c71cdf9d38bfb26aa3e385792e9ac
Tony Stuart <anthonyfstuart@gmail.com>
2015-08-17 07:48:20 -0700

    Factor out Scene and TrackBuilder classes

--- Jamming/src/com/example/afs/jamming/Converter.java
+++ Jamming/src/com/example/afs/jamming/Converter.java
@@ -12,12 +12,8 @@ package com.example.afs.jamming;
 import java.util.List;
 
 import javax.sound.midi.InvalidMidiDataException;
-import javax.sound.midi.MetaMessage;
-import javax.sound.midi.MidiEvent;
-import javax.sound.midi.MidiMessage;
 import javax.sound.midi.Sequence;
 import javax.sound.midi.ShortMessage;
-import javax.sound.midi.Track;
 
 import com.sun.media.sound.MidiUtils;
 
@@ -40,8 +36,8 @@ public class Converter {
   public Sequence convert(List<Block> blocks, int tickCount, int ticksPerBeat, int channel, int program) {
     try {
       Sequence sequence = new Sequence(Sequence.PPQ, ticksPerBeat);
-      Track track = sequence.createTrack();
-      addShortMessage(0, track, channel, ShortMessage.PROGRAM_CHANGE, program, 0);
+      TrackBuilder trackBuilder = new TrackBuilder(sequence.createTrack());
+      trackBuilder.addShortMessage(0, channel, ShortMessage.PROGRAM_CHANGE, program, 0);
       for (Block block : blocks) {
         int left = block.getItem().getLeft();
         int right = block.getItem().getRight();
@@ -59,44 +55,10 @@ public class Converter {
           default:
             throw new UnsupportedOperationException(tickOrigin.toString());
         }
-        int key = block.getKey().getValue();
-        addNote(tick, track, channel, key, velocity, duration);
+        block.getComposable().addToTrack(trackBuilder, tick, channel, velocity, duration);
       }
-      addMetaMessage(tickCount, track, channel, MidiUtils.META_END_OF_TRACK_TYPE, null, 0);
+      trackBuilder.addMetaMessage(tickCount, channel, MidiUtils.META_END_OF_TRACK_TYPE, null, 0);
       return sequence;
-    } catch (InvalidMidiDataException e) {
-      throw new RuntimeException(e);
-    }
-  }
-
-  private void addMessage(long tick, Track track, MidiMessage message) {
-    MidiEvent midiEvent = new MidiEvent(message, tick);
-    track.add(midiEvent);
-  }
-
-  private void addMetaMessage(long tick, Track track, int channel, int type, byte[] data, int length) {
-    try {
-      MetaMessage message = new MetaMessage(type, data, length);
-      addMessage(tick, track, message);
-    } catch (InvalidMidiDataException e) {
-      throw new RuntimeException(e);
-    }
-  }
-
-  private long addNote(long tick, Track track, int channel, int note, int velocity, long duration) {
-    try {
-      addShortMessage(tick, track, channel, ShortMessage.NOTE_ON, note, velocity);
-      addShortMessage(tick + duration, track, channel, ShortMessage.NOTE_OFF, note, 0);
-      return tick;
-    } catch (Exception e) {
-      throw new RuntimeException(e);
-    }
-  }
-
-  private void addShortMessage(long tick, Track track, int channel, int command, int data1, int data2) {
-    try {
-      ShortMessage message = new ShortMessage(command, channel, data1, data2);
-      addMessage(tick, track, message);
     } catch (InvalidMidiDataException e) {
       throw new RuntimeException(e);
     }
