a785a4ed484a95e00f5e6678b646791d0d8837c4
Tony Stuart <anthonyfstuart@gmail.com>
2015-09-06 12:22:22 -0700

    Rationalize 7 chord color maps and descriptive messages

--- Jamming/src/com/example/afs/jamming/Chord7ColorRgbColorMap.java
+++ Jamming/src/com/example/afs/jamming/Chord7ColorRgbColorMap.java
@@ -11,17 +11,13 @@ package com.example.afs.jamming;
 
 public class Chord7ColorRgbColorMap extends RgbColorMap {
 
-  public static final Color beige = new Color("Beige", 0xc79a84);
-  public static final Color blue = new Color("Blue", 0x1c444a);
-  public static final Color brown = new Color("Brown", 0xb16444);
-  public static final Color green = new Color("Green", 0x017451);
-  public static final Color indigo = new Color("Indigo", 0x606b80);
-  public static final Color lemon = new Color("Lemon", 0xc6c477);
-  public static final Color orange = new Color("Orange", 0xbc7c34);
-  public static final Color red = new Color("Red", 0xb21830);
-  public static final Color violet = new Color("Violet", 0x8d433c);
-  public static final Color white = new Color("White", 0xa4b0af);
-  public static final Color yellow = new Color("Yellow", 0xbdbc5e);
+  public static final Color blue = new Color("Blue", 0x00FFFF);
+  public static final Color green = new Color("Green", 0x00FF00);
+  public static final Color indigo = new Color("Indigo", 0x0000FF);
+  public static final Color orange = new Color("Orange", 0xFF8000);
+  public static final Color red = new Color("Red", 0xFF0000);
+  public static final Color violet = new Color("Violet", 0xFF00FF);
+  public static final Color yellow = new Color("Yellow", 0xFFFF00);
 
   // "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B", "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"
   // 0           2          4    5          7          9          11   12         14         16   17         19         21         23
@@ -31,8 +27,8 @@ public class Chord7ColorRgbColorMap extends RgbColorMap {
     add(yellow, new ScaleBasedChord("Emin (iii)", 60, 4, 7, 11));
     add(green, new ScaleBasedChord("F (IV)", 60, 5, 9, 12));
     add(blue, new ScaleBasedChord("G (V)", 60, 7, 11, 14));
-    add(violet, new ScaleBasedChord("Amin (vi)", 60, 9, 12, 16));
-    add(white, new ScaleBasedChord("Bdim (vii\u00B0)", 60, 11, 14, 17));
+    add(indigo, new ScaleBasedChord("Amin (vi)", 60, 9, 12, 16));
+    add(violet, new ScaleBasedChord("Bdim (vii\u00B0)", 60, 11, 14, 17));
   }
 
 }
